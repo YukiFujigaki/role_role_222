5874c95694c16de2c1c02f7465489f284b8ac2eb
Yuki Fujigaki <yuki.fj5@gmail.com>
2021-04-24 19:02:19 +0900

    ✨ロールモデル追加

---
+++ app/models/role_model.rb
@@ -0,0 +1,2 @@
+class RoleModel < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_094805) do
+ActiveRecord::Schema.define(version: 2021_04_24_095940) do
+
+  create_table "role_models", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.string "description"
+    t.string "good_point"
+    t.string "bad_point"
+    t.string "approach"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "user_profiles", charset: "utf8mb4", force: :cascade do |t|
     t.integer "user_id"
