38438078c354b59e8ed4b513811d949ad74336c0
Yuki Fujigaki <yuki.fj5@gmail.com>
2021-04-25 01:20:25 +0900

    ✨ユーザープロフィールの登録、閲覧機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  has_one :user_profile
+
+  class << self
+    def all_role_users(page, per)
+      self.all.order(created_at: :desc).page(page).per(per)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/user_profiles', to: 'user_profiles#index'
+  get '/user_profiles/:id', to: 'user_profiles#show'
+  get '/user_profiles/:id/new', to: 'user_profiles#new'
+  post '/user_profiles/:id/create', to: 'user_profiles#create'
+  get '/user_profiles/:id/edit', to: 'user_profiles#edit'
+  post '/user_profiles/:id/update', to: 'user_profiles#update'
 end
