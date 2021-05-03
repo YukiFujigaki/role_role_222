39009b629eb3465e075e47d172a274a458c45141
Yuki Fujigaki <yuki.fj5@gmail.com>
2021-05-04 00:59:31 +0900

    💚CI動作チェック

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe UserProfile, type: :model do
   subject { UserProfile.new(name: '名前') }
-  it { expect(subject.name).to eq('名前test') }
+  it { expect(subject.name).to eq('名前') }
 end
